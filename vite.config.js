d1d840b06de4c9a23e5425e59dead0af7cab6d17
MSVVSSAINADH <macharlasainadh2506@gmail.com>
2025-02-14 22:38:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/coursera-paradise-nursery-shopping-application/",
+  base: '/paradise-nursery/',
   plugins: [react()],
-})
+});
